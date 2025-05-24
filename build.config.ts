06450782803e5e42ee4963ac45f32d27ed9e887a
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-05-24 04:45:35 +0200

    refactor: remove Prettier configuration, export typedBem function, and add SCSS generation example
    - Deleted Prettier configuration file.
    - Exported the typedBem function from index.ts for better accessibility.
    - Added a new sample.ts file demonstrating the usage of BEM blocks and SCSS generation.
    - Updated tsconfig.json and tsconfig.node.json to include test directory for TypeScript compilation.

--- build.config.ts
+++ build.config.ts
@@ -1,15 +1,16 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-	entries: ['./src/index'],
-	declaration: true,
 	clean: true,
+	declaration: true,
+	entries: ['./src/index.ts'],
+	failOnWarn: true,
 	rollup: {
 		emitCJS: true,
-		inlineDependencies: true,
 		esbuild: {
 			minify: true,
 		},
+		inlineDependencies: true,
 	},
 	sourcemap: true,
 });
